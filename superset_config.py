58c43c2e904f454122e34d680e5c0bbcd9e9b733
Eleboriyez <50270048+Eleboriyez@users.noreply.github.com>
2025-06-04 04:12:03 +0300

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -1,8 +1,24 @@
 OVERRIDE_HTTP_HEADERS = {
     "X-Frame-Options": "ALLOWALL",
 }
-FEATURE_FLAGS =  {
-  "EMBEDDED_SUPERSET": True,
+# Enable the embedded Superset feature
+FEATURE_FLAGS = {
+    “EMBEDDED_SUPERSET”: True
 }
-"GUEST_ROLE_NAME": "Public",
-"GUEST_TOKEN_JWT_EXP_SECONDS": 3000
+
+# Enable Cross-Origin Resource Sharing (CORS)
+ENABLE_CORS = True
+CORS_OPTIONS = {
+    “supports_credentials”: True,
+    “allow_headers”: “*”,
+    “expose_headers”: “*”,
+    “resources”: “*”,
+    “origins”: []  # React URL or frontend URL
+}
+
+# Dashboard Embedding Configuration
+GUEST_ROLE_NAME = “Gamma”
+GUEST_TOKEN_JWT_SECRET = “PASTE_GENERATED_SECRET_HERE”  # Replace with a secure secret
+GUEST_TOKEN_JWT_ALGO = “HS256”
+GUEST_TOKEN_HEADER_NAME = “X-GuestToken”
+GUEST_TOKEN_JWT_EXP_SECONDS = 300  # Token expiry time set to 5 minutes
